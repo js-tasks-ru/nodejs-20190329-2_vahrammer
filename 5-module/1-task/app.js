524164b6b3dd6cb39c6d9c499d03c56e12dd1282
Sergey Vakhrameev <vahrammer@gmail.com>
2019-04-23 19:17:57 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,17 +1,31 @@
+const path = require('path');
+
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(require('koa-static')('public'));
+app.use(require('koa-static')(path.resolve(__dirname, 'public')));
 app.use(require('koa-logger')());
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
 
+const chat = require('./chat');
+
 router.get('/subscribe', async (ctx, next) => {
+  await chat.subscribe(ctx);
 });
 
 router.post('/publish', async (ctx, next) => {
+  const body = ctx.request.body;
+
+  if (!body || !body.message) {
+    ctx.throw(400);
+  }
+
+  chat.send(body.message);
+  ctx.response.type = 'application/json';
+  ctx.body = JSON.stringify({statusText: 'ok'});
 });
 
 app.use(router.routes());
---
+++ 5-module/1-task/chat.js
@@ -0,0 +1,20 @@
+const connections = [];
+
+exports.subscribe = function(ctx) {
+  return new Promise((resolve) => {
+    const connData = {resolve, ctx};
+
+    connections.push(connData);
+    ctx.req.on('close', () => {
+      const pos = connections.indexOf(connData);
+      connections.splice(pos, 1);
+    });
+  });
+};
+
+exports.send = function(message) {
+  connections.forEach(({resolve, ctx}) => {
+    ctx.body = message;
+    resolve();
+  });
+};
